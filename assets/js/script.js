58a3748923014fd251c22ff3884aa84554f7865d
Will Darkins <willdarkins@gmail.com>
2021-08-24 08:06:37 -0700

    form submission fully functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,11 @@ var taskFormHandler = function(event) {
     type: taskTypeInput
   };
 
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  formEl.reset();
 // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
 };
